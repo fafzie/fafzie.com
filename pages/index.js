6f834dc93545cea6d1cd8ce4082388475453b60c
fafzie <fafis@aimedis.com>
2023-06-24 18:19:05 +0400

    new

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import Homeview from '@components/Home'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import ContainerBlock from "../components/ContainerBlock";
+import FavouriteProjects from "../components/FavouriteProjects";
+import LatestCode from "../components/LatestCode";
+import Hero from "../components/Hero";
+import getLatestRepos from "@lib/getLatestRepos";
+import userData from "@constants/data";
 
-export default function Home() {
+export default function Home({ repositories }) {
   return (
+<<<<<<< HEAD
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
@@ -24,5 +29,29 @@ export default function Home() {
       <Footer />
     </div>
   )
+=======
+    <ContainerBlock
+      title="Manu Arora - Developer, Writer, Creator"
+      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+    >
+      <Hero />
+      <FavouriteProjects />
+      <LatestCode repositories={repositories} />
+    </ContainerBlock>
+  );
+>>>>>>> parent of b0ec15c (new)
 }
 
+export const getServerSideProps = async () => {
+  console.log(process.env.GITHUB_AUTH_TOKEN);
+  let token = process.env.GITHUB_AUTH_TOKEN;
+
+  const repositories = await getLatestRepos(userData, token);
+  // console.log("REPOSITORIES", repositories);
+
+  return {
+    props: {
+      repositories,
+    },
+  };
+};
